dd4659e250d3f5343336264fa4909c9bdc304e51
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-27 23:10:12 +0000

    First commit - create project

---
+++ src/main/java/com/giosinosini/springboot2_mongodb/Springboot2MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.giosinosini.springboot2_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giosinosini/springboot2_mongodb/Springboot2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giosinosini.springboot2_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
