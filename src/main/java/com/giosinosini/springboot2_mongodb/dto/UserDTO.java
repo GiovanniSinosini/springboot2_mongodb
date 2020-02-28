38d3cbc7cfc9a9879418b595f1ec4314b337280a
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-28 15:06:00 +0000

    Using DTO standard to return users

---
+++ src/main/java/com/giosinosini/springboot2_mongodb/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.giosinosini.springboot2_mongodb.dto;
+
+import java.io.Serializable;
+
+import com.giosinosini.springboot2_mongodb.domain.User;
+
+public class UserDTO implements Serializable {         //   Data Transfer Object  -  customization of operations
+	private static final long serialVersionUID = 1L;
+
+
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+	
+	public UserDTO(User obj) {
+		
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
--- src/main/java/com/giosinosini/springboot2_mongodb/resources/UserResource.java
+++ src/main/java/com/giosinosini/springboot2_mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.giosinosini.springboot2_mongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giosinosini.springboot2_mongodb.domain.User;
+import com.giosinosini.springboot2_mongodb.dto.UserDTO;
 import com.giosinosini.springboot2_mongodb.services.UserService;
 
 @RestController
@@ -21,9 +23,10 @@ public class UserResource {   //back-end
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){             // ResponseEntity = encapsulate http responses
+	public ResponseEntity<List<UserDTO>> findAll(){             // ResponseEntity = encapsulate http responses
 		List<User> list = service.findAll();    // searches for users in the database 
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());   // convert each list item into DTO object
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	
