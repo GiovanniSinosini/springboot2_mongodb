4deafe3c4696cf22557052cd3175d70fbefef962
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-28 07:13:35 +0000

    Entity User e Rest working

---
+++ src/main/java/com/giosinosini/springboot2_mongodb/resources/UserResource.java
@@ -0,0 +1,28 @@
+package com.giosinosini.springboot2_mongodb.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.giosinosini.springboot2_mongodb.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {   //back-end
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){             // ResponseEntity = encapsulate http responses
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+}
