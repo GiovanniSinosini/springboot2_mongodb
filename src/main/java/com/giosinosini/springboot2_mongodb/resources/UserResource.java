14e6a42ef5acd6d8bba893a2fcde89c5abb00d04
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-02-29 06:04:34 +0000

    User insertion with post

--- src/main/java/com/giosinosini/springboot2_mongodb/resources/UserResource.java
+++ src/main/java/com/giosinosini/springboot2_mongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.giosinosini.springboot2_mongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.giosinosini.springboot2_mongodb.domain.User;
 import com.giosinosini.springboot2_mongodb.dto.UserDTO;
@@ -33,5 +37,13 @@ public class UserResource {   //back-end
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){           
 		User obj = service.findById(id);   
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping                                   // or RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){           
+		User obj = service.fromDTO(objDto); 
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();   // get the address of the new object
+		return ResponseEntity.created(uri).build();     // created = return 201 code
 	}
 }
--- src/main/java/com/giosinosini/springboot2_mongodb/services/UserService.java
+++ src/main/java/com/giosinosini/springboot2_mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.giosinosini.springboot2_mongodb.domain.User;
+import com.giosinosini.springboot2_mongodb.dto.UserDTO;
 import com.giosinosini.springboot2_mongodb.repository.UserRepository;
 import com.giosinosini.springboot2_mongodb.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
