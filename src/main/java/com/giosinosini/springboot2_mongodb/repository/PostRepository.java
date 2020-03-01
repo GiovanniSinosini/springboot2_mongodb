a215cb248dca660cab85f68c67cc560d2d3a1db6
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-03-01 07:26:14 +0000

    Simple query with @Query

--- src/main/java/com/giosinosini/springboot2_mongodb/repository/PostRepository.java
+++ src/main/java/com/giosinosini/springboot2_mongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.giosinosini.springboot2_mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.giosinosini.springboot2_mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {     // to access MongoDB data
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);    // spring data mount the query
 
